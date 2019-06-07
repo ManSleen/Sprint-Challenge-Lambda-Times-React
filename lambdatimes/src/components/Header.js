1e5706ad3b9936fa8afc9ab5b3c988ce175e3153
Mike VanSleen <mike.vansleen@gmail.com>
2019-06-07 10:27:36 -0600

    Added current date to header

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React from "react";
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
+  let d = new Date();
+  let month = d.getMonth();
+  let day = d.getDate();
+  let year = d.getFullYear();
+  let monthArray = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
   return (
     <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+      <span className="date">
+        {monthArray[month]} {day}, {year}
+      </span>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
